5f610beebafead93b72f2606e85a141f72ac16d5
tsomsomm <tsomsomm@gmail.com>
2025-09-05 01:01:08 +0900

    add generated data code

---
+++ Assets/1_Scripts/Generated/GeneratedGameData.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace Generated
+{
+    public class MapData
+    {
+        public string MapResourceKey { get; private set; }
+
+        public MapData(string mapResourceKey)
+        {
+            MapResourceKey = mapResourceKey;
+        }
+    }
+
+    public class TestData
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public float Hp { get; private set; }
+        public ColorType ColorType { get; private set; }
+
+        public TestData(int id, string name, float hp, ColorType colorType)
+        {
+            Id = id;
+            Name = name;
+            Hp = hp;
+            ColorType = colorType;
+        }
+    }
+
+}
