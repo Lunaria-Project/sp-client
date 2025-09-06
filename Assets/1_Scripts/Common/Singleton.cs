2f7ba95c4f3c460a7847fc9ca80a4fec94fbae9c
tsomsomm <tsomsomm@gmail.com>
2025-09-06 21:39:57 +0900

    edit Singleton

--- Assets/1_Scripts/Common/Singleton.cs
+++ Assets/1_Scripts/Common/Singleton.cs
@@ -1,18 +1,23 @@
 public class Singleton<T> where T : Singleton<T>, new()
 {
-    public static T Instance { get; private set; } = GetInstance();
+    public static T Instance => GetInstance();
     public static bool HasInstance => _instance != null;
+
     private static T _instance;
+    private static readonly object _lock = new();
 
     private static T GetInstance()
     {
-        if (!HasInstance)
+        lock (_lock)
         {
-            _instance = new T();
-            _instance.OnInit();
-        }
+            if (!HasInstance)
+            {
+                _instance = new T();
+                _instance.OnInit();
+            }
 
-        return _instance;
+            return _instance;
+        }
     }
 
     protected virtual void OnInit()
