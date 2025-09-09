2c4b69e67da3f4a58403dfcc8587355b5290fdce
tsomsomm <tsomsomm@gmail.com>
2025-09-10 00:45:11 +0900

    add ListExtension

---
+++ Assets/1_Scripts/Common/Extension/ListExtension.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+public static class ListExtension
+{
+    public static bool IsNullOrEmpty<T>(this List<T> list)
+    {
+        return list == null || list.Count == 0;
+    }
+}
