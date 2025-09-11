ab1f3338d353aea48fb362bb820e3824a2e2e90e
tsomsomm <tsomsomm@gmail.com>
2025-09-12 00:08:26 +0900

    debug 코드 제거

--- Assets/1_Scripts/Title/TitleManager.cs
+++ Assets/1_Scripts/Title/TitleManager.cs
@@ -1,13 +1,7 @@
-using UnityEngine;
-
 public class TitleManager : SingletonMonoBehaviour<TitleManager>
 {
     public void Start()
     {
         GameData.Instance.LoadGameData();
-        Debug.Log("LoadGameData");
-        var a = GameData.Instance.DTKeyTestData;
-        var a1 = GameData.Instance.DTMapData;
-        var a2 = GameData.Instance.DTTestData;
     }
 }
