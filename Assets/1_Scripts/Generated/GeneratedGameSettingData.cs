29bdad1797afb1390e41630af74c2c0c8e57449d
tsomsomm <tsomsomm@gmail.com>
2025-09-21 02:05:13 +0900

    update data

--- Assets/1_Scripts/Generated/GeneratedGameSettingData.cs
+++ Assets/1_Scripts/Generated/GeneratedGameSettingData.cs
@@ -3,6 +3,12 @@ using System;
 public class GameSetting : Singleton<GameSetting>
 {
     public int SecondsPerGameHour { get; private set; }
+    public int StartUserHp { get; private set; }
+    public int MaximumUserHp { get; private set; }
+    public int LunitesPerSolari { get; private set; }
+    public int GrainsPerLunite { get; private set; }
+    public int StartUserLunite { get; private set; }
+    public int MaximumSolariCount { get; private set; }
 
     public void InvokeLoadForSheet(SheetInfo sheetInfo)
     {
@@ -12,6 +18,12 @@ public class GameSetting : Singleton<GameSetting>
             switch (name)
             {
                 case "SecondsPerGameHour": SecondsPerGameHour = Convert.ToInt32(row[2]); break;
+                case "StartUserHp": StartUserHp = Convert.ToInt32(row[2]); break;
+                case "MaximumUserHp": MaximumUserHp = Convert.ToInt32(row[2]); break;
+                case "LunitesPerSolari": LunitesPerSolari = Convert.ToInt32(row[2]); break;
+                case "GrainsPerLunite": GrainsPerLunite = Convert.ToInt32(row[2]); break;
+                case "StartUserLunite": StartUserLunite = Convert.ToInt32(row[2]); break;
+                case "MaximumSolariCount": MaximumSolariCount = Convert.ToInt32(row[2]); break;
             }
         }
     }
