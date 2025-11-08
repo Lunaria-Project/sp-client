fbb5f1d6ccf7bfa4bae292aea0378618a065216a
tsomsomm <tsomsomm@gmail.com>
2025-11-09 02:46:00 +0900

    remove TestData

--- Assets/1_Scripts/Generated/GameData.GeneratedClass.cs
+++ Assets/1_Scripts/Generated/GameData.GeneratedClass.cs
@@ -8,18 +8,4 @@ public partial class GameData
     public bool TryGetItemData(int key, out ItemData result) => DTItemData.TryGetValue(key, out result);
     public bool ContainsItemData(int key) => DTItemData.ContainsKey(key);
     private readonly Dictionary<int, ItemData> _dtItemData = new();
-
-    // KeyTestData - KeyTestData, key: Id
-    public IReadOnlyDictionary<int, KeyTestData> DTKeyTestData => _dtKeyTestData;
-    public bool TryGetKeyTestData(int key, out KeyTestData result) => DTKeyTestData.TryGetValue(key, out result);
-    public bool ContainsKeyTestData(int key) => DTKeyTestData.ContainsKey(key);
-    private readonly Dictionary<int, KeyTestData> _dtKeyTestData = new();
-
-    // MapData - MapData
-    public IReadOnlyList<MapData> DTMapData => _dtMapData;
-    private List<MapData> _dtMapData = new();
-
-    // TestData - TestData
-    public IReadOnlyList<TestData> DTTestData => _dtTestData;
-    private List<TestData> _dtTestData = new();
 }
